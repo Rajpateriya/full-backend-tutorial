a8c7cfb4ac2f0f8896310e8366a3cb58ce2801d3
Rajpateriya <pateriyaraj52@gmail.com>
2024-11-18 03:38:48 +0530

    token validation implemented

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 mongoose.connect("mongodb://localhost:27017/tutorial")
 .then(()=>console.log('Mongodb connected'))
@@ -29,6 +34,23 @@ userSchema.pre('save' ,async function(next) {
 })
 
 const User = mongoose.model('User' , userSchema);
+
+
+const verifyToken = (req,res,next) =>{
+    const token = req.cookies.token;
+    if(!token){
+        return res.status(403).json({message : "No token"})
+    }
+
+    jwt.verify(token , process.env.TOKEN , (err,decoded) =>{
+        if(err){
+            return res.status(403).json({message : "No token"})
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
 app.get('/',async(req,res)=>{
  res.json({message : "Running...."})
 })
@@ -40,8 +62,8 @@ app.post('/create' ,async(req,res)=>{
         
         res.status(201).send(user);
     } catch (error) {
-        console.log(error)
-        res.status(500).json({message : "Something went wrong"})
+        console.log(error.message)
+        res.status(500).json({message : error.message})
     }
 })
 
@@ -58,24 +80,42 @@ app.post('/login', async(req,res)=>{
         if(!isMatch){
             res.status(401).json({message : "Credentials not match"})
         }
+
+        const token = jwt.sign({id : user._id, email : user.email} , process.env.TOKEN);
+        res.cookie('token',token);
+
         res.status(200).json({message : "login successfull"})
     } catch (error) {
         console.log(error)
-        res.status(500).json({message : "Something went wrong"})
+        res.status(500).json({message : error.message})
     }
 })
-app.get('/all',async(req,res)=>{
+app.get('/all', verifyToken ,async(req,res)=>{
     try {
         const users = await User.find();
 
         res.status(200).send(users);
     } catch (error) {
         console.log(error)
-        res.status(500).json({message : "Something went wrong"})
+        res.status(500).json({message : error.message})
     }
 })
 
-app.patch('/update/:id' ,async(req,res)=>{
+app.get('/profile/:id' , async(req,res)=>{
+     const id = req.params.id;
+     try {
+         const user = await User.findById(id);
+         if(!user){
+            return res.status(404).json({message : "user not found"})
+         }
+         res.status(200).send(user);
+     } catch (error) {
+        console.log(error)
+        res.status(500).json({message : error.message})
+    }
+})
+
+app.patch('/update/:id' , verifyToken ,async(req,res)=>{
     const {id} = req.params;
     const {name , email , password} = req.body;
     try {
@@ -95,18 +135,20 @@ app.patch('/update/:id' ,async(req,res)=>{
         res.status(200).send(user);
     } catch (error) {
         console.log(error)
-        res.status(500).json({message : "Something went wrong"})
+        res.status(500).json({message : error.message})
     }
 })
-app.delete('/del/:id' ,async(req,res)=>{
+app.delete('/del/:id' , verifyToken ,async(req,res)=>{
     const id = req.params.id;
     try {
+        
         const user = await User.findByIdAndDelete(id);
+        res.clearCookie('token');
         
         res.status(200).json({message : "Deleted"});
     } catch (error) {
         console.log(error)
-        res.status(500).json({message : "Something went wrong"})
+        res.status(500).json({message : error.message})
     }
 })
 
